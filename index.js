a31aa783b26b332ba533a8e46e02110d449e09d3
Ditta123 <udita.jindal1@hp.com>
2018-09-23 19:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("Europe/Copenhagen");
     callback(null, {
         statusCode: '200',
-        body: 'Time in Los Angeles is: ' + currentTime.toString(),
+        body: 'Time in Copenhagen is: ' + currentTime.toString(),
     });
 };
